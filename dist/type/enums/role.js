c0998c59e3d776414bf8e203b303dfbb8bc219e2
roee ltmanovich <roeeli770@gmail.com>
2024-10-13 14:30:53 +0300

    js crete enum for role

---
+++ dist/type/enums/role.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.role = void 0;
+var role;
+(function (role) {
+    role["teacher"] = "teacher";
+    role["student"] = "student";
+})(role || (exports.role = role = {}));
